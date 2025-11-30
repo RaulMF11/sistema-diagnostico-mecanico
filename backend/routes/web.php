c71e723015736d82f38c7d0c3e9b0cf50dd720c6
Diego MF <dmarquezf11@gmail.com>
2025-11-29 22:18:34 -0500

    Eliminar ruta para ejecutar migraciones desde el backend

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Artisan;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,8 +15,4 @@ use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
-});
-Route::get('/run-migrations', function () {
-    Artisan::call('migrate --force');
-    return 'Migraciones ejecutadas';
 });
